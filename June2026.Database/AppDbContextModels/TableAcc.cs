5e55d2e9cccace7d8508ae0f4233a57443c05ac5
Pc <Pc@DESKTOP-LTEFI9I>
2026-07-08 15:08:04 +0630

    feat:AppDbContextModels

---
+++ June2026.Database/AppDbContextModels/TableAcc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace June2026.Database.AppDbContextModels;
+
+public partial class TableAcc
+{
+    public int UserId { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public int Password { get; set; }
+}
